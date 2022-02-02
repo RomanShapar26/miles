98d71c2b14bd997010aff48fad2dc9a1ec0d646d
Роман Шапарь <romanshaparution@gmail.com>
2022-02-02 10:25:41 +0300

    added file

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Входные данные
+        int amount = 140;
+
+        // Логика
+        int bonus = amount / 20 * 1;
+
+    System.out.println("Сумма бонусов за покупку:" + bonus);
+    }
+}
